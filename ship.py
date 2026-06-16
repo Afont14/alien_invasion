3e981ef9d975514817f2f019130d532ca6a11f2e
Afont14 <afont14@gmail.com>
2026-06-15 20:42:23 -0500

    Correcting error involving self.screen_rect()

--- ship.py
+++ ship.py
@@ -7,7 +7,8 @@ class Ship:
     def __init__(self, ai_game):
         """Initialize the ship and set its starting position."""
         self.screen = ai_game.screen
-        self.ettings = ai_game.settings
+        self.settings = ai_game.settings
+        self.screen_rect = ai_game.screen.get_rect()
 
         # Load the ship image and get its rect.
         self.image = pygame.image.load('images/ship.bmp')
